b971b8983dbc524fed95431bedd109be4162baa4
Liu Xiaoyi <circuitcoder0@gmail.com>
2019-05-24 21:31:34 +0800

    Finished lock

--- lock/lock.vhdl
+++ lock/lock.vhdl
@@ -7,14 +7,25 @@ entity lock is
        clk: in std_logic;
        rst: in std_logic;
        succ: out std_logic;
-       fail: out std_logic
+       fail: out std_logic;
+       locked: out std_logic
      );
 end lock;
 
 architecture bhv of lock is
-  type state_type is (IDLE, START, VERIFY_0, VERIFY_1, VERIFY_2, VERIFY_3, SET, SUCCESS);
+  type state_type is (IDLE, ADMIN_0, ADMIN_1, ADMIN_2, ADMIN_3, VERIFY_0, VERIFY_1, VERIFY_2, VERIFY_3, SET_0, SET_1, SET_2, SET_3, SUCCESS);
+  type lock_type is (INITIAL, TRY_1, TRY_2, TRY_3);
   signal state: state_type := IDLE;
-  signal pass: std_logic_vector(3 downto 0) := "0000";
+  signal lock: lock_type := INITIAL;
+  signal pass0: std_logic_vector(3 downto 0) := "0001";
+  signal pass1: std_logic_vector(3 downto 0) := "0010";
+  signal pass2: std_logic_vector(3 downto 0) := "0100";
+  signal pass3: std_logic_vector(3 downto 0) := "1000";
+
+  signal admin0: std_logic_vector(3 downto 0) := "0001";
+  signal admin1: std_logic_vector(3 downto 0) := "0010";
+  signal admin2: std_logic_vector(3 downto 0) := "0100";
+  signal admin3: std_logic_vector(3 downto 0) := "1000";
 begin
   process(state)
   begin
@@ -33,49 +44,112 @@ begin
     end case;
   end process;
 
+  process(lock, state)
+  begin
+    if(lock = TRY_3 and state /= VERIFY_0 and state /= VERIFY_1 and state /= VERIFY_2 and state /= VERIFY_3) then
+      locked <= '1';
+    else
+      locked <= '0';
+    end if;
+  end process;
+
   process(rst, clk)
   begin
     if(rst = '1') then
-      state <= START;
+      if mode = "01" then
+        if lock /= TRY_3 then
+          state <= VERIFY_0;
+
+          case lock is
+            when INITIAL =>
+              lock <= TRY_1;
+            when TRY_1 =>
+              lock <= TRY_2;
+            when others =>
+              lock <= TRY_3;
+          end case;
+        end if;
+      elsif mode = "00" then
+        if state = SUCCESS then
+          state <= SET_0;
+        end if;
+      elsif mode = "10" then
+        state <= ADMIN_0;
+      end if;
     elsif rising_edge(clk) then
       case state is
-        when START =>
-          if mode = "01" then
-            state <= VERIFY_1;
+        when ADMIN_0 =>
+          if admin0 /= input then
+            state <= IDLE;
           else
-            state <= SET;
+            state <= ADMIN_1;
+          end if;
+
+        when ADMIN_1 =>
+          if admin1 /= input then
+            state <= IDLE;
+          else
+            state <= ADMIN_2;
+          end if;
+
+        when ADMIN_2 =>
+          if admin2 /= input then
+            state <= IDLE;
+          else
+            state <= ADMIN_3;
+          end if;
+
+        when ADMIN_3 =>
+          if admin3 /= input then
+            state <= IDLE;
+          else
+            state <= SUCCESS;
+            lock <= INITIAL;
           end if;
 
         when VERIFY_0 =>
-          if pass(0) /= input(0) then
+          if pass0 /= input then
             state <= IDLE;
           else
             state <= VERIFY_1;
           end if;
 
         when VERIFY_1 =>
-          if pass(1) /= input(1) then
+          if pass1 /= input then
             state <= IDLE;
           else
             state <= VERIFY_2;
           end if;
 
         when VERIFY_2 =>
-          if pass(2) /= input(2) then
+          if pass2 /= input then
             state <= IDLE;
           else
             state <= VERIFY_3;
           end if;
 
         when VERIFY_3 =>
-          if pass(3) /= input(3) then
+          if pass3 /= input then
             state <= IDLE;
           else
             state <= SUCCESS;
+            lock <= INITIAL;
           end if;
 
-        when SET =>
-          pass <= input;
+        when SET_0 =>
+          pass0 <= input;
+          state <= SET_1;
+
+        when SET_1 =>
+          pass1 <= input;
+          state <= SET_2;
+
+        when SET_2 =>
+          pass2 <= input;
+          state <= SET_3;
+
+        when SET_3 =>
+          pass3 <= input;
           state <= SUCCESS;
 
         when others =>
--- lock/tb.vhdl
+++ lock/tb.vhdl
@@ -15,52 +15,319 @@ architecture bhv of tb is
        );
   end component;
 
-  signal i: std_logic_vector(3 downto 0);
-  signal m: std_logic_vector(1 downto 0);
-  signal c: std_logic;
-  signal r: std_logic;
+  signal i: std_logic_vector(3 downto 0) := "0000";
+  signal m: std_logic_vector(1 downto 0) := "00";
+  signal c: std_logic := '0';
+  signal r: std_logic := '0';
   signal s: std_logic;
   signal f: std_logic;
 begin
-  process
-  begin
-    loop
-      c <= '1';
-      wait for 0.5 us;
-      c <= '0';
-      wait for 0.5 us;
-    end loop;
-  end process;
-
   l: lock port map(input => i, mode => m, clk => c, rst => r, succ => s, fail => f);
 
   process
   begin
     loop
+      -- Verify default
       m <= "01";
-      i <= "0000";
+      wait for 1 us;
       r <= '1';
       wait for 1 us;
       r <= '0';
-      wait for 10 us;
 
-      i <= "0110";
-      r <= '1';
+      i <= "0001";
       wait for 1 us;
-      r <= '0';
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0010";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0100";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "1000";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
       wait for 10 us;
 
+      -- Set
       m <= "00";
+      wait for 1 us;
       r <= '1';
       wait for 1 us;
       r <= '0';
+
+      i <= "1000";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0100";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0010";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0001";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
       wait for 10 us;
 
+      -- Verify default
       m <= "01";
+      wait for 1 us;
       r <= '1';
       wait for 1 us;
       r <= '0';
+
+      i <= "0001";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0010";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0100";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "1000";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
       wait for 10 us;
+
+      -- Verify default
+      m <= "01";
+      wait for 1 us;
+      r <= '1';
+      wait for 1 us;
+      r <= '0';
+
+      i <= "0001";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0010";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0100";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "1000";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      wait for 10 us;
+
+      -- Verify partial
+      m <= "01";
+      wait for 1 us;
+      r <= '1';
+      wait for 1 us;
+      r <= '0';
+
+      i <= "1000";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0100";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0010";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "1111";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      wait for 10 us;
+
+      -- Now is locked
+
+      -- Correct pass
+      m <= "01";
+      wait for 1 us;
+      r <= '1';
+      wait for 1 us;
+      r <= '0';
+
+      i <= "1000";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0100";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0010";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0001";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      wait for 10 us;
+
+      -- Still locked
+
+      -- Incorrect Admin
+      m <= "10";
+      wait for 1 us;
+      r <= '1';
+      wait for 1 us;
+      r <= '0';
+
+      i <= "0001";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0010";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0100";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "1001";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      wait for 10 us;
+
+      -- Admin
+      m <= "10";
+      wait for 1 us;
+      r <= '1';
+      wait for 1 us;
+      r <= '0';
+
+      i <= "0001";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0010";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0100";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "1000";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      wait for 10 us;
+
+      -- Correct pass
+      m <= "01";
+      wait for 1 us;
+      r <= '1';
+      wait for 1 us;
+      r <= '0';
+
+      i <= "1000";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0100";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0010";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      i <= "0001";
+      wait for 1 us;
+      c <= '1';
+      wait for 1 us;
+      c <= '0';
+
+      wait for 10 us;
+
       std.env.stop(0);
     end loop;
   end process;
